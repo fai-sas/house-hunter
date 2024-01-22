eebb4f22cba6a9570917da1c45136d6fcf4e630b
fai-sas <craftedbyfaisal@gmail.com>
2024-01-22 22:58:29 +0600

    login controller added

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -25,3 +25,28 @@ const register = async (req, res) => {
 
   res.status(StatusCodes.CREATED).json({ user: tokenUser })
 }
+
+const login = async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    throw CustomError.BadRequestError('Please provide email and password')
+  }
+  const user = await User.findOne({ email })
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError('Invalid Credentials')
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password)
+
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError('Incorrect Password')
+  }
+
+  const tokenUser = createTokenUser(user)
+
+  attachCookiesToResponse({ res, user: tokenUser })
+
+  res.status(StatusCodes.OK).json({ user: tokenUser })
+}
