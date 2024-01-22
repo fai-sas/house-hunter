d72f96055860e56ea69c25e60e4928fc0eee493a
fai-sas <craftedbyfaisal@gmail.com>
2024-01-22 22:49:17 +0600

    error middlewares added

---
+++ server/errors/unauthorized.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+const { StatusCodes } = require('http-status-codes')
+const CustomAPIError = require('./custom-api')
+
+class UnauthorizedError extends CustomAPIError {
+  constructor(message) {
+    super(message)
+    this.statusCode = StatusCodes.FORBIDDEN
+  }
+}
+
+module.exports = UnauthorizedError
